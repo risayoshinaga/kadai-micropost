8f2e23b3b0892ca1e2ef3ea6ce1e20d1020d7f3c
taro.kirameki <taro.kirameki@techacademy.jp>
2018-06-12 07:09:32 +0000

    follow, unfollow

--- microposts/app/Http/Controllers/UsersController.php
+++ microposts/app/Http/Controllers/UsersController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\User; // add
+use App\User;
+use App\Micropost;
 
 class UsersController extends Controller
 {
@@ -19,9 +20,15 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        return view('users.show', [
+        $data = [
             'user' => $user,
-        ]);
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
